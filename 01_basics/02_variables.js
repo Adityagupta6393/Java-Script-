1eeeb97870cba63bc99d9eff2c36b791692638a9
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-22 07:56:37 +0000

    Add changes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+///  alert("Helllo Aditya"); we are using nodejs , not browser
+
+console.log(3 
+    + 3)       // code readability should be high
+
+console.log("Aditya");
+
+let name = "Aditya"; // string variable
+let age = 22;      // number variable
+let isLoggedIn = false; // boolean variable
+
+// number => 2 to pwer 53
+// bigint => larger than 2 to power 53
+// string => "" , '' , ``
+// boolean => true/false
+// null => standalone value and has type object
+// undefined => non existent value and has type undefined
+// symbol => unique 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
