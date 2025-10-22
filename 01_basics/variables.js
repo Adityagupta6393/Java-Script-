a829712a4fdc30fab7b912ca68fdd5fd77f1e6cf
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-22 06:44:40 +0000

    Added information in comment

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -4,5 +4,6 @@ var accountPassword = 1233455
 accountCity = "Azamgarh"
 
 console.log(accountId);
+//  Prefer not use var because of issue in block scope and functional scope
 
 console.log([accountEmail, accountPassword, accountCity]);
