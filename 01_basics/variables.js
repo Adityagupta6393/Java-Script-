8c93af2b9014b7f7f3c5eaa477588781c13f8ab9
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-22 06:36:59 +0000

    Add Changes

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 14445;
+let accountEmail = "aditya123@gmail.com"
+var accountPassword = 1233455
+accountCity = "Azamgarh"
+
+console.log(accountId);
+
+console.log([accountEmail, accountPassword, accountCity]);
