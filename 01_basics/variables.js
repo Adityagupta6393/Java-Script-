7fc87a4d317214ac7e4e6b02a18cd8f5c8ffcd0d
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-22 07:11:27 +0000

    Add changes

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,6 +2,8 @@ const accountId = 14445;
 let accountEmail = "aditya123@gmail.com"
 var accountPassword = 1233455
 accountCity = "Azamgarh"
+let aacountNumber;
+console.log(aacountNumber); // It will print undefined because we have not assigned any value to it yet
 
 console.log(accountId);
 //  Prefer not use var because of issue in block scope and functional scope
