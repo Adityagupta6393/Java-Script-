ca90c66b7875887e37022f691ebdcb3ae598a176
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-23 03:01:06 +0000

    conversion Of Data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = Boolean(score);
+
+console.log(typeof isLoggedIn);
+console.log(isLoggedIn);
+
+// 0 => false , 1 => true
+// "" => false , "Aditya" => true
+// null => false , " " => true
+// undefined => false
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
