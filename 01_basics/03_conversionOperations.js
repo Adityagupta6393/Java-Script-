b99c3b9726f6788eefdfb732e529767df88aad3c
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-23 13:18:20 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -14,8 +14,8 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = Boolean(score);
 
-console.log(typeof isLoggedIn);
-console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(isLoggedIn);
 
 // 0 => false , 1 => true
 // "" => false , "Aditya" => true
@@ -26,5 +26,47 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// *****************************Operations*******************************
+
+let value = 33;
+let negValue = -value;
+
+//console.log(negValue); // -33
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Aditya";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+console.log(+true);
+
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+console.log(num1, num2, num3);
+
+// *****************************Increment / Decrement*******************************
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
