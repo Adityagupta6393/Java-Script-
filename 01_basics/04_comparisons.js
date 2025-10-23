bc4b1b7da8b080f21b7356f9958ab1766c0b45bf
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-23 17:36:16 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2 < 3);
+// console.log(2 >= 3);
+// console.log(3 == 3);
+// console.log(3 != 3);
+
+// console.log("2" > 1); 
+// console.log("02" > 1); 
+
+console.log(null > 0);
+console.log(null == 0);  // equality and other relational operators behave differently
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+console.log(NaN > 0);
+console.log(NaN < 0);
+console.log(NaN == 0);
+
+console.log("2" === 2);  // strict equality => It will check both value as well as data type
+console.log("2" !== 2);  // strict not equal => It will check both value as well as data type
+
+
