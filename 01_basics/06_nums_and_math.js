464b644965aa388aa2686965e329b87f2b2c4467
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-24 14:25:06 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,51 @@
+/*const score = 400
+console.log(score);
+console.log(typeof score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(typeof balance);
+
+console.log(balance.toString());  // Now it became a string so it hold all properties and methods of string
+
+console.log(balance.toFixed(2)); // it will convert number to string with 2 decimal places
+
+console.log(balance.toPrecision(3)); // it will convert number to string with total 3 digits
+
+console.log(Number.parseFloat("100.567px")); // it will extract float number from the string
+
+console.log(Number.parseInt("100.567px")); // it will extract integer number from the string
+
+const hundred = 100000;
+
+console.log(hundred.toLocaleString("en-IN")); // it will format number according to the locale provided
+
+//***********************************Math Library******************************************
+
+console.log(Math.PI); // it will give the value of pi
+
+console.log(Math.round(4.6)); // it will round the number to nearest integer
+
+console.log(Math.ceil(4.2)); // it will round the number to next integer
+
+console.log(Math.floor(4.8)); // it will round the number to previous integer
+
+console.log(Math.sqrt(64)); // it will give the square root of the number
+
+console.log(Math.abs(-55)); // it will give the absolute value of the number
+
+console.log(Math.pow(2, 3)); // it will give the power of the number
+
+console.log(Math.min(2, 5, 1, 8, -4)); // it will give the minimum number among the provided numbers
+
+console.log(Math.max(2, 5, 1, 8, -4)); // it will give the maximum number among the provided numbers
+
+console.log(Math.random()); // it will give a random number between 0 and 1 */
+
+console.log(Math.floor(Math.random() * 10) + 1); // it will give a random number between 1 and 10
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // it will give a random number between min and max (both inclusive)
+
