5b3a299d18e46a164e949fe6280692d2d76279d8
Adityagupta6393 <117804124+Adityagupta6393@users.noreply.github.com>
2025-10-26 10:52:06 +0000

    API's Introduction

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -41,10 +41,63 @@ const user = [
     {id: 3, name: "Saksham", age: 19}
 ]
 
-console.log(user[1].name);
+/*console.log(user[1].name);
 
 console.log(Object.keys(regularUser)); // to get all keys of object as array
 console.log(Object.values(regularUser)); // to get all values of object as array
 console.log(Object.entries(regularUser)); // to get all key-value pairs as nested array
 
 console.log(regularUser.hasOwnProperty("age")); // to check if object has a specific property or not
+*/
+
+
+
+// ******************************************************************************************************
+/*const course = {
+    courseName: "JavaScript",
+    price: 999,
+    courseInstructor: "Aditya Gupta",
+}
+
+// We can access object keys as
+console.log(course.courseName);
+console.log(course['courseName']);
+
+//But If we want extract key from object
+const {courseInstructor} = course;  // object destructuring
+console.log(courseInstructor);// Now we can access courseInstructor directly without using course.courseInstructor
+
+// We can also rename while destructuring
+const {courseName: cn} = course;
+console.log(cn); // Now we can access courseName using cn
+
+// We can also set default value while destructuring
+const {rating = 5} = course; // rating key is not present in course object so it will take default value 5
+console.log(rating);
+*/
+
+// *****************************************API*************************************************************
+
+// Before API would get data in form of XML but now most of the APIs use JSON format to send and receive data
+
+// JSON - JavaScript Object Notation
+// It is a lightweight data interchange format that is easy for humans to read and write and easy for machines to parse and generate
+
+// Example of JSON
+const myJson = `{
+    "name": "Aditya",
+    "age": 18,
+    "isLoggedIn": false,
+    "lastLoginDays": ["Monday", "Friday", "Saturday"]
+}`;
+
+// To convert JSON string to JavaScript object we use JSON.parse()
+const myObj = JSON.parse(myJson);
+// console.log(myObj);
+// console.log(myObj.name);
+
+// To convert JavaScript object to JSON string we use JSON.stringify()
+const jsonString = JSON.stringify(myObj);
+// console.log(jsonString);
+// console.log(typeof jsonString); // it will be string
+
